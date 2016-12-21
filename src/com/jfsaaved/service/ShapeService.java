7a68c97b669b51cd052e2f8b358b6d6b612ec064
Julian Saavedra <julian.felipe.saavedra@gmail.com>
2016-12-21 11:51:21 -0500

    December 21, 2016 - Commit 1 - Service and Model

---
+++ src/com/jfsaaved/service/ShapeService.java
@@ -0,0 +1,23 @@
+package com.jfsaaved.service;
+
+import com.jfsaaved.model.Circle;
+import com.jfsaaved.model.Triangle;
+
+public class ShapeService {
+
+	private Circle circle;
+	private Triangle triangle;
+	public Circle getCircle() {
+		return circle;
+	}
+	public void setCircle(Circle circle) {
+		this.circle = circle;
+	}
+	public Triangle getTriangle() {
+		return triangle;
+	}
+	public void setTriangle(Triangle triangle) {
+		this.triangle = triangle;
+	}
+	
+}
