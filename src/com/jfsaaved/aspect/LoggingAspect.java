13c71e0756663c37116e0ae02321a0481f8b7e6e
Julian Saavedra <julian.felipe.saavedra@gmail.com>
2017-01-02 00:06:25 -0500

    January 02, 2017 - Commit 1 - Pointcuts and Wildcards

--- src/com/jfsaaved/aspect/LoggingAspect.java
+++ src/com/jfsaaved/aspect/LoggingAspect.java
@@ -11,9 +11,39 @@ import org.springframework.stereotype.Component;
 @Aspect
 public class LoggingAspect {
 	
-	@Before("execution(public String com.jfsaaved.model.Circle.getName())")
+	
+	// @Before("execution(public String com.jfsaaved.model.Circle.getName())")
+	@Before("allGetters()")
 	public void loggingAdvice() {
 		System.out.println("Advice run. Get method called.");
 	}
+	
+	/*
+	 * Wildcards
+	 * If you want the same advice to applied to different points / different pointcuts
+	 * An advice that applies to multiply methods
+	 * Can add packages in the beginning
+	 * "execution(public * get*())"
+	 * "execution(public * get*())" 0 arguments
+	 * "execution(public * get*(..))" 0 or more arguments
+	 * "execution(public * get*(*))" 1 or more arguments
+	 */
+	
+	@Before("allGetters()")
+	public void secondAdvice() {
+		System.out.println("Second advice ran. Get method called.");
+	}
+	
+	
+	/*
+	 * Pointcuts
+	 * For multiple expressions
+	 * You don't have to use the pattern for each and every advice
+	 * If you want one particular expression to apply for different advice
+	 */
+	
+	@Pointcut("execution(public * get*())")
+	public void allGetters() {}
+
 
 }
